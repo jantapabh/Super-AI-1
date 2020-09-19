21f28dd3ecfd352fcbb4959ca5079d9d4363f89e
jantapabh <s6035512034@phuket.psu.ac.th>
2020-09-19 14:16:09 +0700

    add commit

--- index_part1.py
+++ index_part1.py
@@ -1,8 +1,19 @@
 # Part 2 Date 19/9/63
+# a = 10
+# b = 5
+# if a > b:
+#     print('Correct')
+# else:
+#     print('Hello')    
 
-a = 10
-b = 5
-if a > b:
-    print('Correct')
-else:
-    print('Hello')    
+# a = [1,2,3,4,5,6] = List ใน Python
+# print(a[2]) 
+
+
+# Function
+i = 0
+def iloveyou(ntime):
+    for i in range(ntime):
+        print('i love you')
+
+
